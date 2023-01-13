ec1792ad4d2b8200897c58e6b42a484b8366bbac
ayedun <88404572+ayedun@users.noreply.github.com>
2023-01-12 20:38:07 -0800

    Add computer choice sel

---
+++ javasc.js
@@ -0,0 +1,27 @@
+
+
+function playRound(playerChoice, computerChoice) {
+
+}
+function getPlayerChoice() {
+
+    
+}
+
+
+
+
+
+
+function getComputerChoice() {
+    return myArray [Math.floor(Math.random()*myArray.length)];
+}
+
+
+let myArray = ["rock", "paper", "scissors"]
+let computerChoice = getComputerChoice();
+let playerChoice = getPlayerChoice();
+
+
+
+
